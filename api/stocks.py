35957fb3debd462d9d614a05ba29d8497151ee05
alexlueng <alexlueng000@gmail.com>
2023-03-30 19:37:05 +0800

    add stock models

--- api/stocks.py
+++ api/stocks.py
@@ -1,9 +1,11 @@
 from typing import Tuple, List, Optional
 
 from fastapi import APIRouter, Request, Body, status, HTTPException
-# from fastapi.encoders import jsonable_encoder
+from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
-# from models import CarBase
+from models.stock import StockModel
+from models.response import ResponseModel, ErrorResponseModel
+from datetime import datetime
 
 router = APIRouter()
 
@@ -14,10 +16,14 @@ router = APIRouter()
 
 
 def stock_helper(stock) -> dict:
+
+    # date_string = datetime.strptime(stock["date"], '%Y-%m-%dT%H:%M:%S').date()
+
     return {
-        "id": str(stock["_id"]),
+        "id": str(stock["id"]),
         "close": stock["close"],
-        "date": stock["date"],
+        "volume": stock["volume"],
+        "date": stock["date"].date(),
     }
 
 
@@ -29,21 +35,55 @@ def list_stocks():
 
     return {"stocks": "List of all stocks"}
 
+
+
+"""
+response format 
+{
+    "id": 1,
+    "stock_code": "000001.XSHE",
+    "stock_name": "平安银行", 
+    "history_price": [
+        {
+        "close": 12.00,
+        "volume": 10000000,
+        "date": "2021-01-01"
+        },
+        {
+        "close": 12.00,
+        "date": "2021-01-01"
+        },
+        {
+        "close": 12.00,
+        "date": "2021-01-01"
+        },
+        ...
+    ]
+}
+"""
 @router.get("/{stock_id}", response_description="Get a single stock")
-async def show_stock(stock_id: str, request: Request):
+async def get_one_stock_history_data(stock_id: str, request: Request):
 
     # get a single stock collection from the database and return the result to the user
 
     print(f"stock_id: {stock_id}")
+
+    stock_info = await request.app.mongodb['stock_code'].find_one({"stock_code": stock_id})
     
-    stock_collection = request.app.mongodb.get_collection(stock_id)
+    print(f"stock_info: {type(StockModel(**stock_info))}")
+
+    # return ResponseModel("", "Stock data retrieved successfully")
+    response_data = StockModel(**stock_info)
+    
+    stock_history_price_collection = request.app.mongodb.get_collection(stock_id)
     
     stock_prices = []
-    async for stock in stock_collection.find():
-        stock_prices.append(stock_helper(stock))
+    async for price in stock_history_price_collection.find():
+        stock_prices.append(stock_helper(price))
     
+    response_data.history_data = stock_prices
 
     if stock_prices is not None:
-        return stock_prices
+        return jsonable_encoder(response_data)
     
     return HTTPException(status_code=404, detail=f"Stock {stock_id} not found")
--- models/stock.py
+++ models/stock.py
@@ -1,24 +1,33 @@
+from typing import Optional
 from infra.base_object import MongoBaseModel
+from pydantic import Field
+from datetime import date
 
 
 class StockModel(MongoBaseModel):
     # collection = 'stocks'
+    id: int = Field(...)
+    stock_code: str = Field(...)
+    display_name: str = Field(...)
+    name: str = Field(...)
+    start_date: date = Field(...)
+    end_date: date = Field(...)
+    type: str = Field(...)
+    history_data: Optional[list] = None
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.name = kwargs.get('name')
-        self.symbol = kwargs.get('symbol')
-        self.price = kwargs.get('price')
-        self.change = kwargs.get('change')
-        self.change_percent = kwargs.get('change_percent')
-        self.last_updated = kwargs.get('last_updated')
-
-    def to_dict(self):
-        return {
-            'name': self.name,
-            'symbol': self.symbol,
-            'price': self.price,
-            'change': self.change,
-            'change_percent': self.change_percent,
-            'last_updated': self.last_updated
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        # json_encoders = { ObjectId: str }
+        schema_extra = {
+            "example": {
+                "stock_id": 0,
+                "stock_code": "000001.XSHE",
+                "display_name": "平安银行",
+                "adj_name": "PAYH",
+                "start_date": "2005-01-04",
+                "end_date": "2020-12-31",
+                "type": "stock"
+            }
         }
+    
